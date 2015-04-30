44ba8b80810f90a88ce392fe3ed709f5aea6d530
Akhil <akhil@zenpayroll.com>
2015-04-30 13:47:55 -0700

    Force curl to use IPv4

--- ext/patron/session_ext.c
+++ ext/patron/session_ext.c
@@ -355,7 +355,7 @@ static void set_options_from_request(VALUE self, VALUE request) {
   }
 
   action = SYM2ID(action_name);
-
+  curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
   if (action == rb_intern("get")) {
     VALUE data = rb_iv_get(request, "@upload_data");
     VALUE download_file = rb_iv_get(request, "@file_name");
